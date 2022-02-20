e5182a4bbf3a6f53d5be3f7a21a6cc9c3a7dc958
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2022-02-21 00:26:30 +0100

    player movement fix + started working on shooting stuff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float VMVdistance;
-    public float HMVdistance; //these variables are meant to be MOVEMENT SPEED
+    public float VMVdistance = 2222;
+    public float HMVdistance = 2222; //these variables are meant to be MOVEMENT SPEED
 
     // Start is called before the first frame update
     void Start()
@@ -39,5 +39,10 @@ public class PlayerMovement : MonoBehaviour
             //this.gameObject.GetComponent<Rigidbody>().velocity = new Vector2(this.transform.position.x + HMVdistance, this.transform.position.y);
             this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(HMVdistance * 1f, 0), ForceMode2D.Force);
         }
+    }
+
+    public void Shoot()
+    {
+        //shooting will be here soon :)
     }
 }

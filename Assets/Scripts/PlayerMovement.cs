89a0fcb4cefcdbf11854b0adf927c457d3f2bc7a
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2022-02-21 00:12:51 +0100

    5000 thousand files + movement script :+1:

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //this.gameObject.GetComponent<Rigidbody>().velocity = new Vector2(this.transform.position.x + HMVdistance, this.transform.position.y);
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, VMVdistance * 1), ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            //this.gameObject.GetComponent<Rigidbody>().velocity = new Vector2(this.transform.position.x + HMVdistance, this.transform.position.y);
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, VMVdistance * -1f), ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //this.gameObject.GetComponent<Rigidbody>().velocity = new Vector2(this.transform.position.x + HMVdistance, this.transform.position.y);
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(HMVdistance * -1f, 0), ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            //this.gameObject.GetComponent<Rigidbody>().velocity = new Vector2(this.transform.position.x + HMVdistance, this.transform.position.y);
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(HMVdistance * 1f, 0), ForceMode2D.Force);
+        }
+    }
+}
